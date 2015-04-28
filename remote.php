8a9e2e19c267fdcecbfa99e69c3ca120655978b4
Rodrigo Campos <rodrigo@sdfg.com.ar>
2015-04-28 12:43:02 -0300

    Move the manual outside the public_html
    If not, the manual will be accessible via something like:
    	phplist.org/wp-content/themes/org15/manual
    
    crawled by bots, etc. And this is NOT what we want. We just want an alias to
    this folder, so move it outside the document root is the way to go.

---
+++ remote.php
@@ -0,0 +1,4 @@
+$header = file_get_contents('https://raw.githubusercontent.com/phpList/epub2html/master/header.html');
+$footer = file_get_contents('https://raw.githubusercontent.com/phpList/epub2html/master/footer.html');
+
+
---
+++ update.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+
+wget objavi.booktype.pro/?book=phplist\&server=booki.flossmanuals.net\&mode=epub -O phplist.epub
+unzip -f phplist.epub
